7caf320ecd3d1e6219a50df79116b4cab86193ff
Baptiste Meunier <baptiste@baptiste-meunier.fr>
2018-01-28 16:25:30 +0100

    FIX : Amélioration du tableau pondéré

--- src/Attribution/AttributionHelper.java
+++ src/Attribution/AttributionHelper.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class AttributionHelper {
 
 	private ArrayList<GroupeFinal> groupes;
-//	private static ArrayList<Integer> choixDejaAttrib;
 
 	public AttributionHelper(ArrayList<GroupeFinal> groupes) {
 		this.groupes = groupes;
@@ -14,27 +13,37 @@ public class AttributionHelper {
 	public void run() {
 		ArrayList<Integer> indexChoixEcole = new ArrayList<Integer>();
 		ArrayList<ArrayList<GroupeChoix>> choixEcole = new ArrayList<ArrayList<GroupeChoix>>();
+
 		/** Remplisage du tableau de choix ponderée **/
-		for(int id = 0; id < groupes.size(); id++) {
-			int poids = 1;
-			for(int c: groupes.get(id).getListechoix()) {
-				if(c != 0) {
-					if(indexChoixEcole.contains(c)) {
-						// Ajout à la liste de choixEcole
-						choixEcole.get(indexChoixEcole.indexOf(c)).add(new GroupeChoix(id, poids));
-					} else {
-						// Ajout d'un nouveau choixEcole
-						indexChoixEcole.add(c);
-						ArrayList<GroupeChoix> array = new ArrayList<GroupeChoix>();
-						array.add(new GroupeChoix(id, poids));
-						choixEcole.add(array);
+		boolean finish = false;
+		int i = 0;
+		
+		while(!finish) {
+			for(int id = 0; id < groupes.size(); id++) {
+				ArrayList<Integer> listes = groupes.get(id).getListechoix();
+				if(i < listes.size()) {
+					finish = false;
+					int c = listes.get(i);
+					if(c != 0) {
+						if(indexChoixEcole.contains(c)) {
+							// Ajout à la liste de choixEcole
+							choixEcole.get(indexChoixEcole.indexOf(c)).add(new GroupeChoix(id, i));
+						} else {
+							// Ajout d'un nouveau choixEcole
+							indexChoixEcole.add(c);
+							ArrayList<GroupeChoix> array = new ArrayList<GroupeChoix>();
+							array.add(new GroupeChoix(id, i));
+							choixEcole.add(array);
+						}
 					}
+				}else {
+					finish = true;
 				}
-				poids++;
 			}
+			i++;
 		}
 		
-		for(int i = 0; i < indexChoixEcole.size(); i++) {
+		for(i = 0; i < indexChoixEcole.size(); i++) {
 			System.out.println("Ecole : " + indexChoixEcole.get(i));	
 			int minValue = Integer.MAX_VALUE;
 			ArrayList<Integer> g = new ArrayList<Integer>();
@@ -52,4 +61,5 @@ public class AttributionHelper {
 				groupes.get(g.get(rand)).setChoix(indexChoixEcole.get(i));
 			}
 		}
+	}
 }
